a6ac5802bf3c32066507f374c99413e505b89023
David Buzatto <davidbuzatto@gmail.com>
2023-12-20 19:29:14 -0300

    angry birds (me) pronto kkk

--- jogos/angry-birds/main.c
+++ jogos/angry-birds/main.c
@@ -15,20 +15,30 @@
 #include "include/utils.h"
 
 #define MAX_OBSTACLES 10
+#define PROBE_QUANTITY 10
 
+const int VELOCITY_MULTIPLIER = 40;
 const int LAUNCH_MAX_DISTANCE = 100;
-const float GRAVITY = 5;
+const float GRAVITY = 1;
 
 typedef struct Ball {
     Vector2 pos;
     Vector2 vel;
     int radius;
+    float friction;
+    float elasticity;
+    float rotationAngle;
+    float collisionProbes[PROBE_QUANTITY];
+    Texture2D staringTexture;
+    Texture2D surprisedTexture;
+    Texture2D launchedTexture;
     Color fillColor;
     Color strokeColor;
 } Ball;
 
 typedef struct Obstacle {
     Rectangle rect;
+    bool visible;
     Color fillColor;
     Color strokeColor;
 } Obstacle;
@@ -43,8 +53,8 @@ int yPress;
 int xOffset;
 int yOffset;
 
-float xLaunch;
-float yLaunch;
+float xStart;
+float yStart;
 bool launched;
 double relativeAngle;
 float launchAngle;
@@ -52,6 +62,10 @@ int launchDistance;
 float pi;
 
 int obstacleQuantity;
+
+Texture2D staringTexture;
+Texture2D surprisedTexture;
+Texture2D launchedTexture;
 
 GameWorld gw;
 Ball ball;
@@ -67,6 +81,8 @@ void draw( GameWorld *gw );
 void drawBall( Ball *ball );
 void drawObstacles( Obstacle *obstacles );
 bool coordVsBallIntercept( int x, int y, Ball *ball );
+bool ballVsObstacleIntercept( Ball *ball, Obstacle *obstacle );
+float getProbePosition( Ball *ball, bool forX, int probeIndex, int probeQuantity );
 
 int main( void ) {
 
@@ -74,7 +90,7 @@ int main( void ) {
     const int screenHeight = 450;
 
     SetConfigFlags( FLAG_MSAA_4X_HINT );
-    InitWindow( screenWidth, screenHeight, "Angry Birds - Prof. Dr. David Buzatto" );
+    InitWindow( screenWidth, screenHeight, "Angry Me :D - Prof. Dr. David Buzatto" );
     SetTargetFPS( 60 );    
     createGameWorld();
 
@@ -91,13 +107,17 @@ int main( void ) {
 
 void createGameWorld( void ) {
 
+    staringTexture = LoadTexture( "resources/meStaring.png" );
+    surprisedTexture = LoadTexture( "resources/meSurprised.png" );
+    launchedTexture = LoadTexture( "resources/meLaunched.png" );
+
     xPress = 0;
     yPress = 0;
     xOffset = 0;
     yOffset = 0;
 
-    xLaunch = 200;
-    yLaunch = 200;
+    xStart = 200;
+    yStart = 200;
     launched = false;
     launchAngle = 0;
     relativeAngle = 0;
@@ -107,14 +127,21 @@ void createGameWorld( void ) {
 
     ball = (Ball){
         .pos = { 
-            .x = xLaunch, 
-            .y = yLaunch
+            .x = xStart, 
+            .y = yStart
         },
         .vel = {
             .x = 50,
             .y = -50
         },
-        .radius = 30,
+        .radius = 40,
+        .friction = 0.99,
+        .elasticity = 0.9,
+        .rotationAngle = 0,
+        .collisionProbes = { 0, 45, 90, 135, 180, 225, 270, 315 },
+        .staringTexture = staringTexture,
+        .surprisedTexture = surprisedTexture,
+        .launchedTexture = launchedTexture,
         .fillColor = BLUE,
         .strokeColor = DARKBLUE,
     };
@@ -129,30 +156,33 @@ void createGameWorld( void ) {
         gw.obstacles[obstacleQuantity++] = (Obstacle){
             .rect = {
                 .x = 550,
-                .y = 80 + 130 * i,
+                .y = 100 + 130 * i,
                 .width = 30,
                 .height = 100
             },
+            .visible = true,
             .fillColor = GREEN,
             .strokeColor = DARKGREEN
         };
         gw.obstacles[obstacleQuantity++] = (Obstacle){
             .rect = {
                 .x = 700,
-                .y = 80 + 130 * i,
+                .y = 100 + 130 * i,
                 .width = 30,
                 .height = 100
             },
+            .visible = true,
             .fillColor = YELLOW,
             .strokeColor = GOLD
         };
         gw.obstacles[obstacleQuantity++] = (Obstacle){
             .rect = {
                 .x = 550,
-                .y = 50 + 130 * i,
+                .y = 70 + 130 * i,
                 .width = 180,
                 .height = 30
             },
+            .visible = true,
             .fillColor = RED,
             .strokeColor = MAROON
         };
@@ -162,11 +192,16 @@ void createGameWorld( void ) {
 
 void destroyGameWorld( void ) {
 
+    UnloadTexture( staringTexture );
+    UnloadTexture( surprisedTexture );
+    UnloadTexture( launchedTexture );
+
 }
 
 void inputAndUpdate( GameWorld *gw ) {
 
     Ball *ball = gw->ball;
+    Obstacle *obstacles = gw->obstacles;
 
     if ( IsMouseButtonPressed( MOUSE_BUTTON_LEFT ) ) {
         if ( coordVsBallIntercept( GetMouseX(), GetMouseY(), ball ) ) {
@@ -183,22 +218,22 @@ void inputAndUpdate( GameWorld *gw ) {
             selectedBall->pos.x = GetMouseX() - xOffset;
             selectedBall->pos.y = GetMouseY() - yOffset;
 
-            int c1 = xLaunch - selectedBall->pos.x;
-            int c2 = yLaunch - selectedBall->pos.y;
+            int c1 = xStart - selectedBall->pos.x;
+            int c2 = yStart - selectedBall->pos.y;
 
             launchDistance = sqrt( c1 * c1 + c2 * c2 );
             relativeAngle = atan2( c2, c1 ) + pi;
             launchAngle = pi - relativeAngle;
 
             if ( launchDistance >= LAUNCH_MAX_DISTANCE ) {
-                selectedBall->pos.x = xLaunch + cos( relativeAngle ) * LAUNCH_MAX_DISTANCE;
-                selectedBall->pos.y = yLaunch + sin( relativeAngle ) * LAUNCH_MAX_DISTANCE;
+                selectedBall->pos.x = xStart + cos( relativeAngle ) * LAUNCH_MAX_DISTANCE;
+                selectedBall->pos.y = yStart + sin( relativeAngle ) * LAUNCH_MAX_DISTANCE;
             }
 
-            float vx =  fabs( selectedBall->pos.x - xLaunch ) / LAUNCH_MAX_DISTANCE;
-            float vy = -fabs( selectedBall->pos.y - yLaunch ) / LAUNCH_MAX_DISTANCE;
-            selectedBall->vel.x = vx * cos( launchAngle ) * 80;
-            selectedBall->vel.y = vy * sin( launchAngle ) * 80;
+            float vx =  fabs( selectedBall->pos.x - xStart ) / LAUNCH_MAX_DISTANCE;
+            float vy = -fabs( selectedBall->pos.y - yStart ) / LAUNCH_MAX_DISTANCE;
+            selectedBall->vel.x = vx * cos( launchAngle ) * VELOCITY_MULTIPLIER;
+            selectedBall->vel.y = vy * sin( launchAngle ) * VELOCITY_MULTIPLIER;
 
         }
 
@@ -208,27 +243,59 @@ void inputAndUpdate( GameWorld *gw ) {
 
         if ( selectedBall != NULL ) {
 
-            if ( selectedBall->pos.x != xLaunch && selectedBall->pos.y != yLaunch ) {
+            if ( selectedBall->pos.x != xStart && selectedBall->pos.y != yStart ) {
                 launched = true;
             }
 
-            /*selectedBall->pos.x = xLaunch;
-            selectedBall->pos.y = yLaunch;*/
             selectedBall = NULL;
 
         }
     }
 
     if ( launched ) {
+
+        // verify obstacle collision
+        for ( int i = 0; i < obstacleQuantity; i++ ) {
+            if ( ballVsObstacleIntercept( ball, &obstacles[i] ) ) {
+                obstacles[i].visible = false;
+                ball->vel.x *= ball->elasticity; // kinda of...
+            }
+        }
+
+        // collision with floor
+        if ( ball->pos.y + ball->radius > GetScreenHeight() ) {
+            ball->pos.y = GetScreenHeight() - ball->radius;
+            ball->vel.y = -ball->vel.y;
+            ball->vel.y *= ball->elasticity;
+        }
+
         ball->pos.x += ball->vel.x;
         ball->pos.y += ball->vel.y;
         ball->vel.y += GRAVITY;
+
+        ball->rotationAngle += 5;
+
+        ball->vel.x *= ball->friction;
+        ball->vel.y *= ball->friction;
+
     }
 
     if ( IsMouseButtonPressed( MOUSE_BUTTON_RIGHT ) ) {
         selectedBall = NULL;
-        ball->pos.x = xLaunch;
-        ball->pos.y = yLaunch;
+        ball->pos.x = xStart;
+        ball->pos.y = yStart;
+        ball->rotationAngle = 0;
+        launched = false;
+    }
+
+    if ( IsKeyPressed( KEY_SPACE ) ) {
+        for ( int i = 0; i < obstacleQuantity; i++ ) {
+            obstacles[i].visible = true;
+        }
+        selectedBall = NULL;
+        ball->pos.x = xStart;
+        ball->pos.y = yStart;
+        ball->rotationAngle = 0;
         launched = false;
     }
 
@@ -242,6 +309,11 @@ void draw( GameWorld *gw ) {
     drawBall( gw->ball );
     drawObstacles( gw->obstacles );
 
+    DrawRectangle( 
+        xStart - 10, yStart - gw->ball->radius - 10, 
+        20, 400, 
+        (Color){ .r = 100, .g = 100, .b = 100, .a = 200 } );
+
     EndDrawing();
 
 }
@@ -250,7 +322,7 @@ void drawBall( Ball *ball ) {
     
     if ( !launched ) {
 
-        DrawLine( xLaunch, yLaunch, ball->pos.x, ball->pos.y, BLACK );
+        DrawLine( xStart, yStart, ball->pos.x, ball->pos.y, BLACK );
 
         float px = ball->pos.x;
         float py = ball->pos.y;
@@ -267,37 +339,74 @@ void drawBall( Ball *ball ) {
             vy += GRAVITY;
         }
 
-        px = ball->pos.x;
+        /*px = ball->pos.x;
         py = ball->pos.y;
         vx = ball->vel.x;
         vy = ball->vel.y;
+
         for ( int i = 0; i < iterations; i++ ) {
             DrawCircle( px, py, 5, ball->fillColor );
             px += vx;
             py += vy;
             vy += GRAVITY;
-        }
+        }*/
 
     }
 
-    DrawCircle( ball->pos.x, ball->pos.y, ball->radius, ball->strokeColor );
-    DrawCircle( ball->pos.x, ball->pos.y, ball->radius-2, ball->fillColor );
+    /*DrawCircle( ball->pos.x, ball->pos.y, ball->radius, ball->strokeColor );
+    DrawCircle( ball->pos.x, ball->pos.y, ball->radius-2, ball->fillColor );*/
 
-    char debugData[100];
+    Texture2D textureToDraw = staringTexture;
+
+    if ( selectedBall != NULL ) {
+        textureToDraw = surprisedTexture;
+    } else if ( launched ) {
+        textureToDraw = launchedTexture;
+    }
+
+    DrawTexturePro( 
+        textureToDraw, 
+        (Rectangle){ 
+            .x = 0, 
+            .y = 0, 
+            .width = 420, 
+            .height = 420
+        },
+        (Rectangle){ 
+            .x = ball->pos.x, 
+            .y = ball->pos.y, 
+            .width = ball->radius*2, 
+            .height = ball->radius*2
+        },
+        (Vector2){
+            .x = ball->radius,
+            .y = ball->radius
+        }, ball->rotationAngle, WHITE
+    );
+
+    /*for ( int i = 0; i < PROBE_QUANTITY * 2; i++ ) {
+        float xp = getProbePosition( ball, true, i, PROBE_QUANTITY );
+        float yp = getProbePosition( ball, false, i, PROBE_QUANTITY );
+        DrawCircle( xp, yp, 5, ORANGE );
+    }*/
+
+    /*char debugData[100];
     sprintf( debugData, 
             "x: %d, y: %d\nvx: %.2f, vy: %.2f\nlaunch angle: %.2f", 
             (int) ball->pos.x, (int) ball->pos.y, 
             ball->vel.x, ball->vel.y, 
             toDegrees( launchAngle ) );
-    DrawText( debugData, ball->pos.x, ball->pos.y + ball->radius, 10, BLACK );
+    DrawText( debugData, ball->pos.x, ball->pos.y + ball->radius, 10, BLACK );*/
 
 }
 
 void drawObstacles( Obstacle *obstacles ) {
 
     for ( int i = 0; i < obstacleQuantity; i++ ) {
-        DrawRectangleRec( obstacles[i].rect, obstacles[i].fillColor );
-        DrawRectangleLinesEx( obstacles[i].rect, 2, obstacles[i].strokeColor );
+        if ( obstacles[i].visible ) {
+            DrawRectangleRec( obstacles[i].rect, obstacles[i].fillColor );
+            DrawRectangleLinesEx( obstacles[i].rect, 2, obstacles[i].strokeColor );
+        }
     }
 
 }
@@ -308,5 +417,34 @@ bool coordVsBallIntercept( int x, int y, Ball *ball ) {
     float c2 = y - ball->pos.y;
 
     return c1 * c1 + c2 * c2 <= ball->radius * ball->radius;
+
+}
+
+bool ballVsObstacleIntercept( Ball *ball, Obstacle *obstacle ) {
+
+    if ( obstacle->visible ) {
+
+        for ( int i = 0; i < PROBE_QUANTITY * 2; i++ ) {
+            float xp = getProbePosition( ball, true, i, PROBE_QUANTITY );
+            float yp = getProbePosition( ball, false, i, PROBE_QUANTITY );
+            bool c = CheckCollisionPointRec( (Vector2){ .x = xp, .y = yp }, obstacle->rect );
+            if ( c ) {
+                return true;
+            }
+        }
+
+    }
+
+    return false;
+
+}
+
+float getProbePosition( Ball *ball, bool forX, int probeIndex, int probeQuantity ) {
+
+    float p = forX ? ball->pos.x : ball->pos.y;
+    float t = forX ? cos( toRadians( ball->collisionProbes[probeIndex%probeQuantity] ) ) : 
+                     sin( toRadians( ball->collisionProbes[probeIndex%probeQuantity] ) );
+
+    return p + ball->radius * ( probeIndex / probeQuantity > 0 ? 0.5 : 1 ) * t;
 
 }
