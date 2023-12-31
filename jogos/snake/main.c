5ee5931f7fc1389d7a75d7dea9ad237c177f7c8e
David Buzatto <davidbuzatto@gmail.com>
2023-12-31 10:50:14 -0300

    implementação do jogo snake, falta finalizar

--- jogos/snake/main.c
+++ jogos/snake/main.c
@@ -15,6 +15,18 @@
 #include <time.h>
 #include <assert.h>
 
+/*
+ * TODO LIST:
+ *     - alimentar e contar pontos;
+ *           - ao alimentar, aumentar a cobra em uma peça quando
+ *             a comida chegar ao fim
+ *     - tempo de vida nas comidas
+ *     - ajustar frames por segundo (aumentar fps e contar tempo de movimentação)
+ *     - aumentar velocidade da cobra quando chegar a determinados pontos
+ *       análogo à ideia de níveis
+ *     - finalizar jogo ganhando (não sobrar espaço no tabuleiro)
+ */
+
 /*---------------------------------------------
  * Library headers.
  --------------------------------------------*/
@@ -30,6 +42,9 @@
  --------------------------------------------*/
 #define BOARD_LINES 18
 #define BOARD_COLUMNS 20
+#define BOARD_MAX_SNAKE_FOOD 1
+#define SNAKE_FOOD_QUANTITY 5
+
 
 /*--------------------------------------------
  * Constants. 
@@ -38,12 +53,34 @@ const int BOARD_CELL_WIDTH = 30;
 const Color BOARD_BORDER_COLOR = { 144, 155, 117, 255 };
 const Color BOARD_CENTER_COLOR = { 157, 171, 133, 255 };
 const Color BOARD_CELL_COLOR = { 0, 0, 0, 20 };
+const int BOARD_MARGIN = 10;
+
 const Color SNAKE_BODY_COLOR = { 11, 8, 11, 255 };
-const Color SNAKE_FOOD_COLOR = { 133, 53, 48, 255 };
+const Color SNAKE_FOOD_COLORS[SNAKE_FOOD_QUANTITY] = {
+    { 133, 53, 48, 255 },
+    { 181, 86, 0, 255 },
+    { 67, 138, 43, 255 },
+    { 39, 70, 141, 255 },
+    { 120, 41, 139, 255 }
+};
+const int SNAKE_FOOD_POINTS[SNAKE_FOOD_QUANTITY] = { 5, 10, 15, 20, 25 };
+
+const Color OVERLAY_COLOR = { 0, 0, 0, 150 };
+
+const int SCORE_HEIGHT = 40;
+const int SCORE_MARGIN = 10;
+
 
 /*---------------------------------------------
  * Custom types (enums, structs, unions etc.)
  --------------------------------------------*/
+typedef enum GameState {
+    GAME_STATE_IDLE,
+    GAME_STATE_PLAYING,
+    GAME_STATE_WON,
+    GAME_STATE_LOSE
+} GameState;
+
 typedef enum SnakeDirection {
     SNAKE_GOING_TO_RIGHT,
     SNAKE_GOING_TO_LEFT,
@@ -64,14 +101,30 @@ typedef struct SnakeFood {
     int column;
     int cellWidth;
     Color color;
+    int points;
 } SnakeFood;
+
+typedef struct Score {
+    Vector2 pos;
+    int width;
+    int height;
+    int margin;
+    int points;
+} Score;
 
 typedef struct GameWorld {
     int lines;
     int columns;
     int cellWidth;
+    int margin;
+    Color borderColor;
+    Color centerColor;
+    Color cellColor;
     Snake *snake;
-    SnakeFood *snakeFood;
+    SnakeFood snakeFood[BOARD_MAX_SNAKE_FOOD];
+    int snakeFoodQuantity;
+    Score *score;
+    GameState state;
 } GameWorld;
 
 
@@ -81,6 +134,7 @@ typedef struct GameWorld {
 GameWorld gw;
 Snake snake;
 SnakeFood snakeFood;
+Score score;
 
 
 /*---------------------------------------------
@@ -98,8 +152,10 @@ void inputAndUpdate( GameWorld *gw );
  */
 void draw( const GameWorld *gw );
 
-void drawSnake( const Snake *snake );
-void drawSnakeFood( const SnakeFood *snakeFood );
+void drawSnake( const Snake *snake, int hOffset, int vOffset );
+void drawSnakeFood( const SnakeFood *snakeFood, int hOffset, int vOffset );
+void drawScore( const Score *score );
+SnakeFood newSnakeFood( void );
 
 /**
  * @brief Create the global Game World object and all of its dependecies.
@@ -124,8 +180,8 @@ void unloadResources( void );
 
 int main( void ) {
 
-    const int screenWidth = BOARD_COLUMNS * BOARD_CELL_WIDTH;
-    const int screenHeight = BOARD_LINES * BOARD_CELL_WIDTH;
+    const int screenWidth = BOARD_COLUMNS * BOARD_CELL_WIDTH + BOARD_MARGIN * 2;
+    const int screenHeight = BOARD_LINES * BOARD_CELL_WIDTH + SCORE_HEIGHT + BOARD_MARGIN * 2;
 
     SetConfigFlags( FLAG_MSAA_4X_HINT );
     InitWindow( screenWidth, screenHeight, "Snake" );
@@ -153,41 +209,80 @@ void inputAndUpdate( GameWorld *gw ) {
     Vector2 temp = {0};
     Vector2 previous = {0};
 
-    if ( IsKeyPressed( KEY_LEFT ) ) {
-        snake->direction = SNAKE_GOING_TO_LEFT;
-    } else if ( IsKeyPressed( KEY_RIGHT ) ) {
-        snake->direction = SNAKE_GOING_TO_RIGHT;
-    } else if ( IsKeyPressed( KEY_UP ) ) {
-        snake->direction = SNAKE_GOING_TO_UP;
-    } else if ( IsKeyPressed( KEY_DOWN ) ) {
-        snake->direction = SNAKE_GOING_TO_DOWN;
-    }
-
-    for ( int i = 0; i < snake->bodyPieces; i++ ) {
-        Vector2 temp = snake->body[i];
-        if ( i == 0 ) {
-            switch ( snake->direction ) {
-                case SNAKE_GOING_TO_LEFT:
-                    snake->body[i].x--;
-                    break;
-                case SNAKE_GOING_TO_RIGHT:
-                    snake->body[i].x++;
-                    break;
-                case SNAKE_GOING_TO_UP:
-                    snake->body[i].y--;
-                    break;
-                case SNAKE_GOING_TO_DOWN:
-                    snake->body[i].y++;
-                    break;
-            }
-        } else {
-            snake->body[i].x = previous.x;
-            snake->body[i].y = previous.y;
+    if ( gw->state == GAME_STATE_IDLE ) {
+        if ( GetKeyPressed() != 0 ) {
+            gw->state = GAME_STATE_PLAYING;
         }
-        previous = temp;
+    } else if ( gw->state == GAME_STATE_WON || gw->state == GAME_STATE_LOSE ) {
+        if ( GetKeyPressed() != 0 ) {
+            destroyGameWorld();
+            createGameWorld();
+            gw->state = GAME_STATE_PLAYING;
+        }
+    } else if ( gw->state == GAME_STATE_PLAYING ) {
+
+        if ( IsKeyPressed( KEY_LEFT ) ) {
+            if ( snake->direction != SNAKE_GOING_TO_RIGHT ) {
+                snake->direction = SNAKE_GOING_TO_LEFT;
+            }
+        } else if ( IsKeyPressed( KEY_RIGHT ) ) {
+            if ( snake->direction != SNAKE_GOING_TO_LEFT ) {
+                snake->direction = SNAKE_GOING_TO_RIGHT;
+            }
+        } else if ( IsKeyPressed( KEY_UP ) ) {
+            if ( snake->direction != SNAKE_GOING_TO_DOWN ) {
+                snake->direction = SNAKE_GOING_TO_UP;
+            }
+        } else if ( IsKeyPressed( KEY_DOWN ) ) {
+            if ( snake->direction != SNAKE_GOING_TO_UP ) {
+                snake->direction = SNAKE_GOING_TO_DOWN;
+            }
+        }
+
+        for ( int i = 0; i < snake->bodyPieces; i++ ) {
+            Vector2 *current = &snake->body[i];
+            Vector2 temp = *current;
+            if ( i == 0 ) {
+                switch ( snake->direction ) {
+                    case SNAKE_GOING_TO_LEFT:
+                        current->x--;
+                        break;
+                    case SNAKE_GOING_TO_RIGHT:
+                        current->x++;
+                        break;
+                    case SNAKE_GOING_TO_UP:
+                        current->y--;
+                        break;
+                    case SNAKE_GOING_TO_DOWN:
+                        current->y++;
+                        break;
+                }
+                if ( current->x < 0 ) {
+                    current->x = gw->columns-1;
+                }
+                if ( current->y < 0 ) {
+                    current->y = gw->lines-1;
+                }
+                current->x = ( (int) current->x ) % gw->columns;
+                current->y = ( (int) current->y ) % gw->lines;
+            } else {
+                current->x = previous.x;
+                current->y = previous.y;
+            }
+            previous = temp;
+        }
+
+        // bite itself
+        Vector2 *head = &snake->body[0];
+        for ( int i = 1; i < snake->bodyPieces; i++ ) {
+            if ( head->x == snake->body[i].x &&
+                 head->y == snake->body[i].y ) {
+                gw->state = GAME_STATE_LOSE;
+            }
+        }
+
     }
 
-    
 
 }
 
@@ -196,54 +291,101 @@ void draw( const GameWorld *gw ) {
     BeginDrawing();
     ClearBackground( WHITE );
 
+    int hOffset = gw->margin;
+    int vOffset = gw->score->height + gw->margin;
+
     DrawRectangleGradientH( 
         0, 0, 
         GetScreenWidth() / 2, GetScreenHeight(), 
-        BOARD_BORDER_COLOR, BOARD_CENTER_COLOR );
+        gw->borderColor, gw->centerColor );
     DrawRectangleGradientH( 
         GetScreenWidth() / 2, 0, 
         GetScreenWidth() / 2, GetScreenHeight(), 
-        BOARD_CENTER_COLOR, BOARD_BORDER_COLOR );
+        gw->centerColor, gw->borderColor );
+
+    DrawRectangleLines( 
+        gw->margin/2, 
+        gw->margin/2 + gw->score->height, 
+        GetScreenWidth() - gw->margin, 
+        GetScreenHeight() - vOffset, BLACK );
 
     for ( int i = 0; i < gw->lines; i++ ) {
         for ( int j = 0; j < gw->columns; j++ ) {
             DrawRectangleLinesEx( 
                 (Rectangle){
-                    .x = j * gw->cellWidth + 2,
-                    .y = i * gw->cellWidth + 2,
+                    .x = j * gw->cellWidth + 2 + hOffset,
+                    .y = i * gw->cellWidth + 2 + vOffset,
                     .width = gw->cellWidth - 2,
                     .height = gw->cellWidth - 2
                 },
                 2,
-                BOARD_CELL_COLOR
+                gw->cellColor
             );
             DrawRectangle(  
-                j * gw->cellWidth + 6,
-                i * gw->cellWidth + 6,
+                j * gw->cellWidth + 6 + hOffset,
+                i * gw->cellWidth + 6 + vOffset,
                 gw->cellWidth - 10,
                 gw->cellWidth - 10,
-                BOARD_CELL_COLOR
+                gw->cellColor
             );
         }
     }
 
-    drawSnake( gw->snake );
-    drawSnakeFood( gw->snakeFood );
+    drawSnake( gw->snake, hOffset, vOffset );
+    for ( int i = 0; i < gw->snakeFoodQuantity; i++ ) {
+        drawSnakeFood( &gw->snakeFood[i], hOffset, vOffset );
+    }
+    drawScore( gw->score );
+
+    if ( gw->state != GAME_STATE_PLAYING ) {
+        DrawRectangle( 0, 0, GetScreenWidth(), GetScreenHeight(), OVERLAY_COLOR );
+    }
+
+    if ( gw->state == GAME_STATE_IDLE ) {
+        const char *m = "tecle algo para começar.";
+        int w = MeasureText( m, 20 );
+        DrawText( m, GetScreenWidth() - w - 10, vOffset + 2, 20, BLACK );
+        DrawText( m, GetScreenWidth() - w - 12, vOffset, 20, WHITE );
+    } else if ( gw->state == GAME_STATE_WON ) {
+
+        const char *m1 = "você ganhou!";
+        int w = MeasureText( m1, 20 );
+        DrawText( m1, GetScreenWidth() - w - 10, vOffset + 2, 20, BLACK );
+        DrawText( m1, GetScreenWidth() - w - 12, vOffset, 20, LIME );
+
+        const char *m2 = "tecle algo para recomeçar.";
+        w = MeasureText( m2, 20 );
+        DrawText( m2, GetScreenWidth() - w - 10, vOffset + 22, 20, BLACK );
+        DrawText( m2, GetScreenWidth() - w - 12, vOffset + 20, 20, WHITE );
+
+    } else if ( gw->state == GAME_STATE_LOSE ) {
+        
+        const char *m1 = "você perdeu...";
+        int w = MeasureText( m1, 20 );
+        DrawText( m1, GetScreenWidth() - w - 10, vOffset + 2, 20, BLACK );
+        DrawText( m1, GetScreenWidth() - w - 12, vOffset, 20, MAROON );
+
+        const char *m2 = "tecle algo para recomeçar.";
+        w = MeasureText( m2, 20 );
+        DrawText( m2, GetScreenWidth() - w - 10, vOffset + 22, 20, BLACK );
+        DrawText( m2, GetScreenWidth() - w - 12, vOffset + 20, 20, WHITE );
+
+    }
 
     EndDrawing();
 
 }
 
-void drawSnake( const Snake *snake ) {
+void drawSnake( const Snake *snake, int hOffset, int vOffset ) {
 
-    for ( int i = 0; i < snake->bodyPieces; i++ ) {
+    for ( int i = snake->bodyPieces-1; i >=0; i-- ) {
         
         const Vector2 *c = &snake->body[i];
 
         DrawRectangleLinesEx( 
             (Rectangle){
-                .x = c->x * snake->cellWidth + 2,
-                .y = c->y * snake->cellWidth + 2,
+                .x = c->x * snake->cellWidth + 2 + hOffset,
+                .y = c->y * snake->cellWidth + 2 + vOffset,
                 .width = snake->cellWidth - 2,
                 .height = snake->cellWidth - 2
             },
@@ -252,8 +394,8 @@ void drawSnake( const Snake *snake ) {
         );
 
         DrawRectangle(  
-            c->x * snake->cellWidth + 6,
-            c->y * snake->cellWidth + 6,
+            c->x * snake->cellWidth + 6 + hOffset,
+            c->y * snake->cellWidth + 6 + vOffset,
             snake->cellWidth - 10,
             snake->cellWidth - 10,
             snake->color
@@ -263,12 +405,12 @@ void drawSnake( const Snake *snake ) {
 
 }
 
-void drawSnakeFood( const SnakeFood *snakeFood ) {
+void drawSnakeFood( const SnakeFood *snakeFood, int hOffset, int vOffset ) {
 
     DrawRectangleLinesEx( 
         (Rectangle){
-            .x = snakeFood->column * snakeFood->cellWidth + 2,
-            .y = snakeFood->line * snakeFood->cellWidth + 2,
+            .x = snakeFood->column * snakeFood->cellWidth + 2 + hOffset,
+            .y = snakeFood->line * snakeFood->cellWidth + 2 + vOffset,
             .width = snakeFood->cellWidth - 2,
             .height = snakeFood->cellWidth - 2
         },
@@ -277,12 +419,31 @@ void drawSnakeFood( const SnakeFood *snakeFood ) {
     );
 
     DrawRectangle(  
-        snakeFood->column * snakeFood->cellWidth + 6,
-        snakeFood->line * snakeFood->cellWidth + 6,
+        snakeFood->column * snakeFood->cellWidth + 6 + hOffset,
+        snakeFood->line * snakeFood->cellWidth + 6 + vOffset,
         snakeFood->cellWidth - 10,
         snakeFood->cellWidth - 10,
         snakeFood->color
     );
+
+}
+
+void drawScore( const Score *score ) {
+    const char *t = TextFormat( "%05d", score->points );
+    DrawText( t, score->pos.x + score->margin, score->pos.y + score->margin, 30, BLACK );
+}
+
+SnakeFood newSnakeFood( void ) {
+
+    int p = GetRandomValue( 0, SNAKE_FOOD_QUANTITY-1 );
+
+    return (SnakeFood) {
+        .line = GetRandomValue( 0, BOARD_LINES-1 ),
+        .column = GetRandomValue( 0, BOARD_COLUMNS-1 ),
+        .cellWidth = BOARD_CELL_WIDTH,
+        .color = SNAKE_FOOD_COLORS[p],
+        .points = SNAKE_FOOD_POINTS[p],
+    };
 
 }
 
@@ -292,59 +453,48 @@ void createGameWorld( void ) {
 
     snake = (Snake) {
         .cellWidth = BOARD_CELL_WIDTH,
-        .body = { 
-            (Vector2){ 
-                .x = 2, 
-                .y = 8 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 7 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 6 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 5
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 4 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 3 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 2 
-            },
-            (Vector2){ 
-                .x = 2, 
-                .y = 1
-            }
-        },
-        .bodyPieces = 8,
-        .direction = SNAKE_GOING_TO_DOWN,
+        .body = {0},
+        .bodyPieces = 0,
+        .direction = SNAKE_GOING_TO_RIGHT,
         .color = SNAKE_BODY_COLOR
     };
 
-    snakeFood = (SnakeFood) {
-        .line = 9,
-        .column = 5,
-        .cellWidth = BOARD_CELL_WIDTH,
-        .color = SNAKE_FOOD_COLOR
+    for ( int i = 0; i < 8; i++ ) {
+        snake.body[snake.bodyPieces++] = (Vector2){ 
+            .x = BOARD_COLUMNS / 2 - i, 
+            .y = BOARD_LINES / 2 - 1
+        };
+    }
+
+    score = (Score) {
+        .pos = {
+            .x = 0,
+            .y = 0
+        },
+        .width = GetScreenWidth(),
+        .height = SCORE_HEIGHT,
+        .margin = SCORE_MARGIN,
+        .points = 0
     };
 
     gw = (GameWorld) {
         .lines = BOARD_LINES,
         .columns = BOARD_COLUMNS,
         .cellWidth = BOARD_CELL_WIDTH,
+        .margin = BOARD_MARGIN,
+        .borderColor = BOARD_BORDER_COLOR,
+        .centerColor = BOARD_CENTER_COLOR,
+        .cellColor = BOARD_CELL_COLOR,
         .snake = &snake,
-        .snakeFood = &snakeFood
+        .snakeFood = {0},
+        .snakeFoodQuantity = 0,
+        .score = &score,
+        .state = GAME_STATE_IDLE
     };
+
+    for ( int i = 0; i < BOARD_MAX_SNAKE_FOOD; i++ ) {
+        gw.snakeFood[gw.snakeFoodQuantity++] = newSnakeFood();
+    }
 
 }
 
