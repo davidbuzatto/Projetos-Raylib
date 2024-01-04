65f0e252cb428ec04c32e26c383bd442605ce74e
David Buzatto <davidbuzatto@gmail.com>
2024-01-04 00:15:16 -0300

    desenvolvendo packman

--- jogos/pacman/main.c
+++ jogos/pacman/main.c
@@ -27,28 +27,52 @@
 /*---------------------------------------------
  * Macros. 
  --------------------------------------------*/
-
+#define MAX_MAP_GRAPH_VERTICES 100
 
 /*--------------------------------------------
  * Constants. 
  -------------------------------------------*/
-
+const int MARGIN = 40;
+const int TILE_WIDTH = 25;
 
 /*---------------------------------------------
  * Custom types (enums, structs, unions etc.)
  --------------------------------------------*/
+typedef enum PlayerDirection {
+    PLAYER_DIRECTION_GOING_TO_RIGHT,
+    PLAYER_DIRECTION_GOING_TO_DOWN,
+    PLAYER_DIRECTION_GOING_TO_LEFT,
+    PLAYER_DIRECTION_GOING_TO_UP
+} PlayerDirection;
+
 typedef struct Point2 {
     int x;
     int y;
 } Point2;
 
+typedef struct Player {
+    int originVertex;
+    int targetVertex;
+    int currentVertex;
+    PlayerDirection direction;
+    Vector2 pos;
+    Vector2 vel;
+    float speed;
+    float radius;
+    float startAngle;
+    float endAngle;
+    bool moving;
+    Color color;
+} Player;
+
 typedef struct Map {
-    int graph[100][4];
-    Point2 vertices[100];
+    int graph[MAX_MAP_GRAPH_VERTICES][4];      // 4: 0-right, 1-down, 2-left, 3-up
+    Point2 vertices[MAX_MAP_GRAPH_VERTICES];
     int verticesQuantity;
 } Map;
 
 typedef struct GameWorld {
+    Player *player;
     Map *map;
 } GameWorld;
 
@@ -57,6 +81,7 @@ typedef struct GameWorld {
  * Global variables.
  --------------------------------------------*/
 GameWorld gw;
+Player player;
 Map map;
 
 
@@ -75,8 +100,11 @@ void inputAndUpdate( GameWorld *gw );
  */
 void draw( const GameWorld *gw );
 
-void parseMapFile( const char *mapPath, Map *map );
+void parseMapFile( const char *mapStructurePath, const char *mapGraphPath, Map *map );
+void addMapGraphEdge( int v, int w, int d, Map *map );
+
 void drawMap( const Map *map );
+void drawPlayer( const Player *player );
 
 /**
  * @brief Create the global Game World object and all of its dependecies.
@@ -101,8 +129,8 @@ void unloadResources( void );
 
 int main( void ) {
 
-    const int screenWidth = 800;
-    const int screenHeight = 800;
+    const int screenWidth = MARGIN * 2 + TILE_WIDTH * 26;
+    const int screenHeight = MARGIN * 2 + TILE_WIDTH * 29;
 
     // turn antialiasing on (if possible)
     SetConfigFlags( FLAG_MSAA_4X_HINT );
@@ -127,14 +155,47 @@ int main( void ) {
 
 void inputAndUpdate( GameWorld *gw ) {
 
+    Player *player = gw->player;
+    Map *map = gw->map;
+
+    if ( IsKeyPressed( KEY_RIGHT ) ) {
+        player->direction = PLAYER_DIRECTION_GOING_TO_RIGHT;
+        player->vel.x = player->speed;
+        player->vel.y = 0;
+        player->moving = true;
+    } else if ( IsKeyPressed( KEY_DOWN ) ) {
+        player->direction = PLAYER_DIRECTION_GOING_TO_DOWN;
+        player->vel.x = 0;
+        player->vel.y = player->speed;
+        player->moving = true;
+    } else if ( IsKeyPressed( KEY_LEFT ) ) {
+        player->direction = PLAYER_DIRECTION_GOING_TO_LEFT;
+        player->vel.x = -player->speed;
+        player->vel.y = 0;
+        player->moving = true;
+    } else if ( IsKeyPressed( KEY_UP ) ) {
+        player->direction = PLAYER_DIRECTION_GOING_TO_UP;
+        player->vel.x = 0;
+        player->vel.y = -player->speed;
+        player->moving = true;
+    }
+
+    // vertex resolution
+    if ( player->moving ) {
+        player->pos.x += player->vel.x;
+        player->pos.y += player->vel.y;
+    }
+    
+
 }
 
 void draw( const GameWorld *gw ) {
 
     BeginDrawing();
-    ClearBackground( WHITE );
+    ClearBackground( BLACK );
 
     drawMap( gw->map );
+    drawPlayer( gw->player );
 
     EndDrawing();
 
@@ -144,20 +205,63 @@ void drawMap( const Map *map ) {
 
     for ( int i = 0; i < map->verticesQuantity; i++ ) {
         const Point2 *p = &map->vertices[i];
-        DrawCircle( 20 + p->x * 20, 20 + p->y * 20, 5, GREEN );
+        DrawCircle( MARGIN + p->x * TILE_WIDTH, MARGIN + p->y * TILE_WIDTH, 10, WHITE );
+        DrawText( TextFormat( "%d", i ), MARGIN + p->x * TILE_WIDTH + 10, MARGIN + p->y * TILE_WIDTH + 10, 20, GRAY );
+    }
+
+    for ( int i = 0; i < map->verticesQuantity; i++ ) {
+        const Point2 *pv = &map->vertices[i];
+        Color c = BLACK;
+        for ( int j = 0; j < 4; j++ ) {
+            int vOffset = 0;
+            int hOffset = 0;
+            switch ( j ) {
+                case 0: c = BLUE;   vOffset = -2; break;
+                case 1: c = LIME;   hOffset = 2;  break;
+                case 2: c = RED;    vOffset = 2;  break;
+                case 3: c = ORANGE; hOffset = -2; break;
+                default: break;
+            }
+            if ( map->graph[i][j] != -1 ) {
+                const Point2 *pw = &map->vertices[map->graph[i][j]];
+                DrawLineEx( 
+                    (Vector2){MARGIN + pv->x * TILE_WIDTH + hOffset, MARGIN + pv->y * TILE_WIDTH + vOffset}, 
+                    (Vector2){MARGIN + pw->x * TILE_WIDTH + hOffset, MARGIN + pw->y * TILE_WIDTH + vOffset}, 
+                    2, c );
+                DrawPoly( (Vector2){MARGIN + pw->x * TILE_WIDTH + hOffset, MARGIN + pw->y * TILE_WIDTH + vOffset}, 3, 4, j * 90, c );
+            }
+        }
     }
 
 }
 
-void parseMapFile( const char *mapPath, Map *map ) {
+void drawPlayer( const Player *player ) {
 
-    char *data = LoadFileText( mapPath );
+    DrawCircleSector( player->pos, player->radius, 
+                      player->startAngle, player->endAngle,
+                      30, player->color );
+
+}
+
+void parseMapFile( const char *mapStructurePath, const char *mapGraphPath, Map *map ) {
+
+    char *structureData = LoadFileText( mapStructurePath );
+    char *graphData = LoadFileText( mapGraphPath );
+
     int currentLine = 0;
     int currentColumn = 0;
+    char c;
 
-    while ( *data != '\0' ) {
+    char buffer[100];
+    int bc = 0;
 
-        char c = *data;
+    int v;    // origin
+    int w;    // target
+    char d;   // direction
+
+    while ( *structureData != '\0' ) {
+
+        c = *structureData;
 
         if ( c == '\n' ) {
             currentLine++;
@@ -172,31 +276,82 @@ void parseMapFile( const char *mapPath, Map *map ) {
             currentColumn++;
         }
 
-        data++;
+        structureData++;
 
     }
 
-    for ( int i = 0; i < map->verticesQuantity; i++ ) {
-        Point2 *p = &map->vertices[i];
-        int line = p->y;
-        int column = p->x;
+    while ( *graphData != '\0' ) {
+
+        c = *graphData;
+
+        if ( c != '\n' ) {
+            buffer[bc++] = c;
+        } else {
+            buffer[bc] = '\0';
+            sscanf( buffer, "%d-%c-%d", &v, &d, &w );
+            addMapGraphEdge( v, w, d, map );
+            bc = 0;
+        }
+
+        graphData++;
+
     }
+
+    sscanf( buffer, "%d-%c-%d", &v, &d, &w );
+    addMapGraphEdge( v, w, d, map );
+
+}
+
+void addMapGraphEdge( int v, int w, int d, Map *map ) {
+
+    int p1 = 0;
+    int p2 = 0;
+
+    switch ( d ) {
+        case 'r': p1 = 0; p2 = 2; break;
+        case 'd': p1 = 1; p2 = 3; break;
+        case 'l': p1 = 2; p2 = 0; break;
+        case 'u': p1 = 3; p2 = 1; break;
+        default: break;
+    }
+
+    map->graph[v][p1] = w;
+    map->graph[w][p2] = v;
 
 }
 
 void createGameWorld( void ) {
-
-    printf( "creating game world...\n" );
 
     map = (Map) {
         .graph = {0},
         .vertices = {0},
         .verticesQuantity = 0
     };
+    memset( map.graph, -1, MAX_MAP_GRAPH_VERTICES * 4 * sizeof( int ) );
 
-    parseMapFile( "resources/map.txt", &map );
+    parseMapFile( "resources/mapStructure.txt", "resources/mapGraph.txt", &map );
+
+    player = (Player) {
+        .originVertex = 0,
+        .targetVertex = -1,
+        .currentVertex = 0,
+        .direction = PLAYER_DIRECTION_GOING_TO_RIGHT,
+        .pos = {0},
+        .vel = {0},
+        .speed = 5,
+        .radius = TILE_WIDTH,
+        .startAngle = 45,
+        .endAngle = 315,
+        .moving = false,
+        .color = YELLOW
+    };
+    player.pos = (Vector2) {
+        .x = MARGIN + map.vertices[player.currentVertex].x * TILE_WIDTH,
+        .y = MARGIN + map.vertices[player.currentVertex].y * TILE_WIDTH,
+    };
 
     gw = (GameWorld) {
+        .player = &player,
         .map = &map
     };
 
